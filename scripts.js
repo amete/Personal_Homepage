36659e3fc0361adf2e60f51d7dc2d791fe7a4fef
amete <serhanmete@gmail.com>
2018-11-23 17:17:51 +0100

    Bug fix

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// Add smooth scrolling to all links
+$(document).ready(function(){
+  $(".scroll-down").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
